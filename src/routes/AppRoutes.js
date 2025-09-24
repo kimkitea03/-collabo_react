4cce47ee208d2543d205eb566ba0d4f51fddcece
김기태 <aqz103@naver.com>
2025-09-24 17:04:40 +0900

    collabo_react 2025/09/24

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -8,6 +8,7 @@ import ElementList from './../pages/ElementList';
 import SignupPage from './../pages/SignupPage';
 import LoginPage from '../pages/LoginPage';
 import ProductList from '../pages/ProductList';
+import ProductInsertForm from '../pages/ProductInsertForm';
 
 
 // 이 파일은 라우팅 정보를 담고 있는 파일입니다.
@@ -24,6 +25,7 @@ function App({ user, handleLoginSuccess }) {
 
             {/* 로그인 여부에 따라서 상품 목록 페이지가 다르게 보여야 하므로, user 프롭스를 넘겨 줍니다. */}
             <Route path='/product/list' element={<ProductList user={user} />} />
+            <Route path='/product/insert' element={<ProductInsertForm user={user} />} />
 
             <Route path='/fruit' element={<FruitOne />} />
             <Route path='/fruit/list' element={<FruitList />} />

4a15732b4a46b7c72941e0c994a1025f0c3a63d0
김기태 <aqz103@naver.com>
2025-09-26 18:01:30 +0900

    collabo_react 2025/09/26

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -112,7 +112,9 @@ function App({ user }) {
                 {/* products는 상품 배열, item은 상품 1개를 의미 */}
                 {products.map((item) => (
                     <Col key={item.id} md={4} className="mb-4">
-                        <Card className="h-100" style={{ cursor: 'pointer' }}>
+                        <Card className="h-100"
+                        onClick={()=>Navigate(`/product/detail/${item.id}`)}
+                        style={{ cursor: 'pointer' }}>
                             <Card.Img
                                 variant="top"
                                 src={`${API_BASE_URL}/images/${item.image}`}
